4ca469c19e797fb4835668c97fd48f00747083c1
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-21 17:00:35 +0200

    premier commit

---
+++ 18compression-python/main.py
@@ -0,0 +1,11 @@
+import random
+import itertools
+
+
+def hello(name):
+    print(f"hello {name}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    hello("elsa")
