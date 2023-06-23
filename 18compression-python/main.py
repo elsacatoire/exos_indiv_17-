422694fd506974b34c649ebbbaae915cfaafd48d
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-23 16:30:57 +0200

    set test and skeleton function for compression

--- 18compression-python/main.py
+++ 18compression-python/main.py
@@ -1,9 +1,5 @@
 
 
-def hello(name):
-    print(f"hello {name}")
-
-
 def cut_text(text_to_cut):
     result = text_to_cut.split(' ')
     return result
@@ -22,17 +18,11 @@ def replace_some_words(words_to_check, swapping_words):
     return words_to_check
 
 
+def compress_words(words_to_compress):
+    return words_to_compress
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    hello("elsa")
-    to_cut = "qu'elle est... » ). expert en utilisabilité des"
-    to_join = ["qu'elle", "est...", "»", ").", "expert", "en", "utilisabilité", "des"]
-    cut = cut_text(to_cut)
-    uncut = reconstruction_sentence(to_join)
-    print(cut)
-    print(uncut)
-    if uncut == to_cut:
-        print("oh yeah")
-
     new_list = replace_some_words(["chat", "rat", "lion", "hello"], {"chat": "1", "quoi": "2", "lion": "12"})
     print(new_list)
--- 18compression-python/tests.py
+++ 18compression-python/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from main import cut_text, reconstruction_sentence, replace_some_words
+from main import cut_text, reconstruction_sentence, replace_some_words, compress_words
 
 
 class PhraseUtilsTests(unittest.TestCase):
@@ -35,6 +35,14 @@ class PhraseUtilsTests(unittest.TestCase):
         dictionary = {'texte': '1', 'lorem': '2', 'qui': '3', 'donc': '4', 'est': '5', 'que': '6', 'pour': '7', 'ceci': '8', 'faux-texte': '9', 'dans': '10', 'plus': '11', 'avec': '12'}
         result = replace_some_words(words_to_swap, dictionary)
         expected = ['mais', '8', '5', 'un', 'long', '9']
+        self.assertEqual(result, expected)
+
+
+    def test_compress_words(self):
+        # Test case 1
+        words_to_compress = ['mais', 'ceci', 'est', 'un', 'long', 'faux-texte']
+        result = compress_words(words_to_compress)
+        expected = 'mais 8 5 un long 9'
         self.assertEqual(result, expected)
 
 
