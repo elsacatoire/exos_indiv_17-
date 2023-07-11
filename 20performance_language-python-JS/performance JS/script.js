a26ab347c017a29252e2d45bb5c030c4acc87442
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-11 18:11:52 +0200

    first commit with function to get time

---
+++ 20performance_language-python-JS/performance JS/script.js
@@ -0,0 +1,14 @@
+
+/* elsa_des_bois
+exercice individuel : performance des languages - version JavaScript */
+
+/* Declaring things here */
+
+function now() { 
+    return new Date
+}
+
+
+/* We run things here */
+currentTime = now()
+console.log(currentTime)
---
+++ 20performance_language-python-JS/performance Pyhton/main.py
@@ -0,0 +1,19 @@
+
+""" elsa_des_bois
+exercice individuel : performance des languages - version JavaScript"""
+
+"""Ipporting things"""
+
+"""Declaring things here"""
+
+from datetime import datetime
+
+def now():
+    return datetime.now()
+
+
+
+""" We run things here """
+if __name__ == '__main__':
+    currentTime = now()
+    print(currentTime)
