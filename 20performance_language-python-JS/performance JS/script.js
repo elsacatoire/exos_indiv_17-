a6a6b2fcc37f025f465997405641e59911d2d036
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-12 17:02:13 +0200

    finish oerformance test

--- 20performance_language-python-JS/performance JS/script.js
+++ 20performance_language-python-JS/performance JS/script.js
@@ -4,11 +4,44 @@ exercice individuel : performance des languages - version JavaScript */
 
 /* Declaring things here */
 
+// getiint time in miliseconds (int)
 function now() { 
-    return new Date
+    const getNow = new Date
+    return getNow.getTime()
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function add_array(array, number_to_add) {
+    const newArray = array.map(x => x + number_to_add)
+    return newArray
+}
+
+function fact(nbr) {
+    if (nbr === 0)
+    {
+        return 1;
+    }
+  return nbr * fact(nbr-1);
 }
 
 
 /* We run things here */
-currentTime = now()
-console.log(currentTime)
+// start
+const initial_time = now()
+
+// code to evaluate
+const sum = add(3, 4)
+console.log(sum);
+
+const addedArrey = add_array([3, 4, 1, 10], 1)
+console.log(addedArrey);
+
+console.log(fact(5));
+
+// code performance evaluation in milisecondes
+const final_time = now()
+const time_to_process = final_time - initial_time
+console.log("L'éxécution à durée " + time_to_process + " miliseconds");
--- 20performance_language-python-JS/performance Pyhton/main.py
+++ 20performance_language-python-JS/performance Pyhton/main.py
@@ -1,3 +1,4 @@
+import time
 
 """ elsa_des_bois
 exercice individuel : performance des languages - version JavaScript"""
@@ -8,12 +9,42 @@ exercice individuel : performance des languages - version JavaScript"""
 
 from datetime import datetime
 
+# getting time in miliseconds (int)
 def now():
-    return datetime.now()
+    return int(time.time() * 1000)
 
+
+def add(a, b) :
+    return a+b
+
+
+def add_array(array, int_to_add) :
+    new_array = []
+    for i in array :
+        new_array.append(i + int_to_add)
+    print(new_array)
+    return new_array
+
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n  * fact(n-1)
 
 
 """ We run things here """
 if __name__ == '__main__':
-    currentTime = now()
-    print(currentTime)
+    initial_time = now()
+    print(initial_time)
+
+    '''...execution du code...'''
+    print(add(3,4))
+    print(add_array([3, 4, 1, 10], 1))
+    print(fact(5))
+
+    """ calcul de performance en milisecondes """
+    final_time = now()
+    print(final_time)
+    time_to_process =  final_time - initial_time
+    print(f"L'éxécution a duré {time_to_process} miliseconds")
