eabc7922d492eae8bfad08508852d79168ea3c8e
Elsa Catoire <elsa.catoire@gmail.com>
2023-08-10 11:45:21 +0200

    tests sur première étapes

--- 23tests-python/main.py
+++ 23tests-python/main.py
@@ -7,11 +7,12 @@ def place_random_bombe(nb_bombs, nb_columns, nb_lines):
     for i in range(nb_bombs):
         x = random.randint(0, nb_columns - 1)
         y = random.randint(0, nb_lines - 1)
+
         grid[y][x] = 'X'
 
 
 def grid_generator(n, m, char):
-    array = [[char for column in range(n)] for line in range(m)]
+    array = [[char for column in range(abs(n))] for line in range(abs(m))]
     print(array)
     return array
 
@@ -26,6 +27,7 @@ def case(x, y):
     if 0 <= x < len(grid):
         if 0 <= y < len(grid[x]):
             playerGrid[y][x] = grid[y][x]
+            return True
         else:
             print("Out of the grid! Play again")
     else:
@@ -35,10 +37,11 @@ def case(x, y):
 if __name__ == "__main__":
     columns = 5
     lines = 10
-    grid = grid_generator(columns, lines, 0)
-    playerGrid = grid_generator(columns, lines, "-")
+    grid = grid_generator(columns, lines, "Z")
+    player_grid = grid_generator(columns, lines, "-")
 
     place_random_bombe(1, columns, lines)
+    print(grid)
 
     while True:
         print("Enter coordinates to open a case")
@@ -46,4 +49,4 @@ if __name__ == "__main__":
         coordinateY = input("Enter y between 1 to " + str(lines) + " : ")
 
         case(int(coordinateX)-1, int(coordinateY)-1)
-        display_grid(playerGrid)
+        display_grid(player_grid)
--- 23tests-python/test_main.py
+++ 23tests-python/test_main.py
@@ -1,6 +1,6 @@
 
 import unittest
-from main import grid_generator
+from main import grid_generator, place_random_bombe
 
 
 class TestGridGenerator(unittest.TestCase):
@@ -14,8 +14,6 @@ class TestGridGenerator(unittest.TestCase):
     def test_grid_no(self):
         columns = 5
         lines = 10
-        inexact_grid = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
         inexact_grid = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
         self.assertNotEqual(grid_generator(columns, lines, 0), inexact_grid)
 
@@ -25,6 +23,19 @@ class TestGridGenerator(unittest.TestCase):
         result = grid_generator(columns, lines, 0)
         self.assertEqual(len(result), lines)
         self.assertEqual(len(result[0]), columns)
+
+    def test_grid_neg_nb(self):
+        columns = -5
+        lines = 10
+        result = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        self.assertEqual(grid_generator(columns, lines, 0), result)
+
+    def test_bomb_grid(self):
+        columns = 5
+        lines = 10
+        grid = grid_generator(columns, lines, "Z")
+        place_random_bombe(1, columns, lines)
 
 
 
